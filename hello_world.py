ac6abca9d0d3eb23b3082d7ddc80e46b18ff9cca
FarisW <Farisalahmad01@hotmail.com>
2022-09-26 13:57:48 -0700

    New rename

--- hello_world.py
+++ hello_world.py
@@ -16,3 +16,19 @@ fave_food2 = "pizza"
 print( "I love to eat {} and {}.".format(fave_food1 , fave_food2)  ) # with .format()
 print( f"I love to eat {fave_food1} and {fave_food2}."  ) # with an f string
 
+
+
+#TASK 2 
+fill = "Hello World"
+hello = "Hello"
+name = "Fwayne"
+numb = 23
+food_one = "burgers"
+food_two = "makloobah"
+
+print(fill)
+print(hello , name , "!")
+print(f'{hello} {name}')  
+print(hello + str(numb) ,"!")
+print( "I love to eat {} and {}.".format(food_one , food_two)  )
+print( f"I love to eat {food_one} and {food_two}."  ) 
